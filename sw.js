8958087bcd579b2efb34c595e0b1db8e79ff8bff
gkkm8bm7rn-eng <gkkm8bm7rn@privaterelay.appleid.com>
2026-08-04 14:50:40 +0300

    Precache the first-visit catalog cache bootstrap

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ const CORE_ASSETS=[
   './',
   './index.html',
   './offline.html',
-  './performance-bootstrap.js?v=3'
+  './performance-bootstrap.js?v=4'
 ];
 
 self.addEventListener('install',event=>{
